b2f26fde16550f5d9b45f714b994378fa14e9bf7
Thomas Papillon <thooms.pap@gmail.com>
2013-05-29 22:39:08 +0200

    Added gui.py

---
+++ source/gui.py
@@ -0,0 +1,9 @@
+"""
+gui.py
+
+This module contains the code necessary to have a graphical interface
+(if chosen).  It relies on pyQt. The idea is to simply print the state
+of game (and the game log if needed for instance), to get user inputs,
+say a menu action, and to interact with the core modules to tell'em to
+modify the game state for example.
+"""
